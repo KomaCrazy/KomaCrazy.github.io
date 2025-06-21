43775c361a49a0a940dffb469f5ecb12d57a59d7
KomaCrazy <zxloveninek90@gmail.com>
2025-06-21 20:23:39 +0700

    init

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
     console.log( "ready!" );
+    MakeContent();
 });
+
+function SetBG(){
+
+}
+
+function MakeContent(){
+    let list = [
+        ContentBasic(),
+    ]
+    for (let i = 0; i < list.length;i++){
+        $("body").append(list[i]);
+    }
+}
+
+
+
+function ContentBasic(){
+    return "<h1>Hello world</h1>"
+}
+
